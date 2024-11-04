02634d9b9ffd29ef75eca4876a93589f20305529
Grzegorz Kozera <01171661@pw.edu.pl>
2024-11-04 15:01:19 +0100

    2 zejsciowka

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,4 +5,22 @@
 class ResourceManager
 {
     // Twoja implementacja tutaj
+    private:
+    Resource resource;
+
+    public:
+    ResourceManager() : resource() {}
+
+    double get() { return resource.get(); }
+
+    ResourceManager(const ResourceManager& other) : resource(other.resource) {}
+
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this != &other) {
+            resource = other.resource;
+        }
+        return *this;
+    }
+
+    ~ResourceManager() { }
 };
